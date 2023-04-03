a6d4ce1b36b8f79792d7313827a32898c538b6dc
Haina Karon <hainakaron.dev@gmail.com>
2023-04-03 14:55:20 -0500

    improved code
    - improved add_entry_to_stats to handle adding new items to groups
    - renamed dict_merge to obj_merge to handle other iterable objects (list) or numbers
    - obj_merge now accepts list and can handle merging two objects with values of different type.
    - added additional tests as well

--- stats.py
+++ stats.py
@@ -1,38 +1,44 @@
 #Note: 'The `entry` dictionary can contain groups (e.g. currency, country_code) that are not yet in the `stats`.')
 def add_entry_to_stats(stats: dict, entry: dict) -> dict:
-    stats['count'] += 1
+    entry['count'] = 1
 
-    for key, val in entry.items(): #str, int
-        prop = { #assuming all new groups have this property
-            'count' : 1, 
-            'amount' : entry['amount'], 
-            'num_items' : entry['num_items']
-        }
+    item_stat = {} #for new group items
+    for key in stats:
+        if type(stats[key]) == int:
+            item_stat[key] = entry[key]
+            stats[key] += entry[key]
 
-        if key in stats and isinstance(stats[key], int): #property
-            stats[key] += val
-        elif key in stats and isinstance(stats[key], dict): #group
-            group_stat = stats[key][val]
-            group_stat = dict_merge(group_stat, prop)
-            stats[key][val] = group_stat
-        else:
-            stats[key] = { val : prop }
+    for key, val in entry.items():
+        if key not in item_stat:
+            stats[key] = obj_merge( stats[key], { val: item_stat} ) if key in stats else { val: item_stat}
+    
     return stats
 
-#Note: 'It has to support nested objects of any depth.'
+#Note: 'It has to support nested objects of any depth.'.
 def merge_stats(*args: dict) -> dict:
     stats = {}
     for stat in args:
-        stats = dict_merge(stats, stat)
+        stats = obj_merge(stats, stat)
     return stats
 
-def dict_merge( stat1, stat2 ):
+def obj_merge( stat1: dict, stat2: dict ) -> dict:
     for k, v in stat2.items():
-        if (k in stat1 and isinstance(stat1[k], dict) and isinstance(v, dict)):  #noqa
-            dict_merge(stat1[k], v)
-        elif (k in stat1 and isinstance(stat1[k], int) and isinstance(v, int)):
-            stat1[k] += v
+        if ( k in stat1 ):
+            temp = stat1[k]
+            
+            addable_types = (int, float)#, complex)
+            
+            if isinstance(temp, dict) and isinstance(v, dict): #dict + dict
+                obj_merge(temp, v)
+            elif type(temp) in addable_types and type(v) in (int, float): #numbers + numbers
+                stat1[k] += v
+            #additional cases
+            elif isinstance(temp, list) and isinstance(v, list): #list + list
+                for i in v:
+                    if i not in temp: #assuming stats1[k] already have unique items
+                        stat1[k] += v
+            elif type(temp) != type(v):
+                stat1[k] = v
         else:
             stat1[k] = v
     return stat1
-
--- tests.py
+++ tests.py
@@ -49,10 +49,55 @@ class TestTask1(unittest.TestCase):
         updated_stats = add_entry_to_stats(stats, entry)
 
         self.assertDictEqual(updated_stats, expected_stats)
+    
+    def test_add_entry_item_to_stats(self):
+        stats = {
+            "count": 16,
+            "amount": 442,
+            "num_items": 14,
+            "gender": {
+                "F": {"count": 5, "amount": 225, "num_items": 7},
+                "M": {"count": 11, "amount": 217, "num_items": 7},
+            },
+            "currency": {
+                "EUR": {"count": 14, "amount": 192, "num_items": 8},
+                "USD": {"count": 2, "amount": 250, "num_items": 6},
+            },
+            "week_of_year": {42: {"count": 1, "amount": 17, "num_items": 2}},
+            "country_code": {"GB": {"count": 1, "amount": 17, "num_items": 2}},
+        }
 
+        entry = {
+            "gender": "F",
+            "amount": 16,
+            "num_items": 3,
+            "currency": "USD",
+            "week_of_year": 16, #new group item
+            "country_code": "GB",
+        }
 
+        expected_stats = {
+            "count": 16+1,
+            "amount": 442+16,
+            "num_items": 14+3,
+            "gender": {
+                "F": {"count": 5+1, "amount": 225+16, "num_items": 7+3},
+                "M": {"count": 11, "amount": 217, "num_items": 7},
+            },
+            "currency": {
+                "EUR": {"count": 14, "amount": 192, "num_items": 8},
+                "USD": {"count": 2+1, "amount": 250+16, "num_items": 6+3},
+            },
+            "week_of_year": {
+                42: {"count": 1, "amount": 17, "num_items": 2},
+                16: {"count": 1, "amount": 16, "num_items": 3}
+            },
+            "country_code": {"GB": {"count": 1+1, "amount": 17+16, "num_items": 2+3}},
+        }
+        updated_stats = add_entry_to_stats(stats, entry)
+        self.assertDictEqual(updated_stats, expected_stats)
+    
 class TestTask2(unittest.TestCase):
-
     maxDiff = None
 
     def test_merge_stats(self):
@@ -103,7 +148,7 @@ class TestTask2(unittest.TestCase):
             "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": 1}}},
             "empty_object": {},
         }
-
+        
         expected_stats = {
             "count": 45,
             "amount": 1300,
@@ -126,7 +171,7 @@ class TestTask2(unittest.TestCase):
 
         self.assertDictEqual(merged_stats, expected_stats)
 
-    def test_merge_stats2(self):
+    def test_merge_stats_with_diff_type(self):
         stats1 = {
             "count": 15,
             "amount": 425,
@@ -153,7 +198,7 @@ class TestTask2(unittest.TestCase):
                 "EUR": {"count": 13, "amount": 175, "num_items": 6},
                 "USD": {"count": 2, "amount": 250, "num_items": 6},
             },
-            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": 41, "mineral": {"gold": {"24k": 5}}}}},
+            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": 41}}},
             "empty_object": {},
         }
 
@@ -171,18 +216,10 @@ class TestTask2(unittest.TestCase):
                 "USD": {"count": 2, "amount": 250, "num_items": 6},
             },
             "country_code": {"GB": {"count": 2, "amount": 10, "num_items": 2}},
-            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": 1, "mineral": {}}}},
+            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": { 'minerals': [ 'gold', 'diamond' ]}}}},
             "empty_object": {},
         }
-
-        stats4 = {
-            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": 1, "mineral": {"gold":{"21k": 3}, "diamond":4}}}},
-        }
-
-        stats5 = {
-            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": 1, "mineral": {"gold":{"21k": 2}, "diamond":3}}}},
-        }
-
+        
         expected_stats = {
             "count": 45,
             "amount": 1300,
@@ -197,13 +234,84 @@ class TestTask2(unittest.TestCase):
                 "USD": {"count": 6, "amount": 750, "num_items": 18},
             },
             "country_code": {"GB": {"count": 2, "amount": 10, "num_items": 2}},
-            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": 44, "mineral": {"gold":{"21k": 5, "24k":5}, "diamond":7}}}},
+            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": { 'minerals': [ 'gold', 'diamond' ]}}}},
             "empty_object": {},
         }
 
-        merged_stats = merge_stats(stats1, stats2, stats3, stats4, stats5)
+        merged_stats = merge_stats(stats1, stats2, stats3)
 
         self.assertDictEqual(merged_stats, expected_stats)
 
+    def test_merge_stats_with_lists(self):
+        stats1 = {
+            "count": 15,
+            "amount": 425,
+            "num_items": 12,
+            "gender": {
+                "F": {"count": 5, "amount": 225, "num_items": 7},
+                "M": {"count": 10, "amount": 200, "num_items": 5},
+            },
+            "currency": {
+                "EUR": {"count": 13, "amount": 175, "num_items": 6},
+                "USD": {"count": 2, "amount": 250, "num_items": 6},
+            },
+        }
+
+        stats2 = {
+            "count": 15,
+            "amount": 425,
+            "num_items": 12,
+            "gender": {
+                "F": {"count": 5, "amount": 225, "num_items": 7},
+                "M": {"count": 10, "amount": 200, "num_items": 5},
+            },
+            "currency": {
+                "EUR": {"count": 13, "amount": 175, "num_items": 6},
+                "USD": {"count": 2, "amount": 250, "num_items": 6},
+            },
+            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": { 'minerals' : [ 'copper' ]}}}},
+            "empty_object": {},
+        }
+
+        stats3 = {
+            "count": 15,
+            "amount": 450,
+            "num_items": 12,
+            "something_else": 30,
+            "gender": {
+                "F": {"count": 5, "amount": 225, "num_items": 7},
+                "M": {"count": 10, "amount": 200, "num_items": 5},
+            },
+            "currency": {
+                "EUR": {"count": 13, "amount": 175, "num_items": 6},
+                "USD": {"count": 2, "amount": 250, "num_items": 6},
+            },
+            "country_code": {"GB": {"count": 2, "amount": 10, "num_items": 2}},
+            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": { 'minerals': [ { 'gold' : [ '21k', '18k']}, 'diamond' ]}}}},
+            "empty_object": {},
+        }
+        
+        expected_stats = {
+            "count": 45,
+            "amount": 1300,
+            "num_items": 36,
+            "something_else": 30,
+            "gender": {
+                "F": {"count": 15, "amount": 675, "num_items": 21},
+                "M": {"count": 30, "amount": 600, "num_items": 15},
+            },
+            "currency": {
+                "EUR": {"count": 39, "amount": 525, "num_items": 18},
+                "USD": {"count": 6, "amount": 750, "num_items": 18},
+            },
+            "country_code": {"GB": {"count": 2, "amount": 10, "num_items": 2}},
+            "something_deep": {"deeper": {"the_deepest": {"hidden_treasure": { 'minerals': [ 'copper', { 'gold' : [ '21k', '18k']}, 'diamond' ]}}}},
+            "empty_object": {},
+        }
+
+        merged_stats = merge_stats(stats1, stats2, stats3)
+
+        self.assertDictEqual(merged_stats, expected_stats)
+    
 if __name__ == "__main__":
     unittest.main()
